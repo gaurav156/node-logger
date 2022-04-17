dfddf750a205ac3781fbe4b03e2af8ec5b3484b3
Gaurav Shukla <gaurav.shukla@global.internal>
2022-04-17 12:41:55 +0530

    adds client-vue

---
+++ client-vue/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+// const logger = require("C:/Users/gaurav.shukla/Documents/WebDev/vue/node-logger/logger");
+import logger from 'C:/Users/gaurav.shukla/Documents/WebDev/vue/node-logger/logger'
+
+createApp(App).use(logger).mount('#app')
